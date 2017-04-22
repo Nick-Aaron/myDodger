23e31c5e734b9d25cf780fc214dd47ce762e2afe
Nick-Aaron <1553533356@qq.com>
2017-04-22 23:05:41 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from distutils.core import setup 
 import glob 
 import py2exe 	  
-setup(console=["main.py"],
-      data_files=[("resources",
-                   ['resources\\background.wav',
-                    'resources\\baddie.png',
-                    'resources\\gameOver.wav',
-                    'resources\\player.png',
-                    'resources\\simsunb.ttf'])])
+setup(windows = ["main.py"],
+      data_files = [("resources",
+                    ['resources\\background.wav',
+                     'resources\\baddie.png',
+                     'resources\\gameOver.wav',
+                     'resources\\player.png',
+                     'resources\\simsunb.ttf'])])
 
